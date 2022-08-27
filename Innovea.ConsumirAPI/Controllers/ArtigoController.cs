62ffa590ef262feabb1c639213fcce9a69b27310
Ariane Melo <ariane.admquirino@gmail.com>
2022-08-26 21:43:01 -0300

    Add project files.

---
+++ Innovea.ConsumirAPI/Controllers/ArtigoController.cs
@@ -0,0 +1,47 @@
+﻿using Innovea.ConsumirAPI.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Innovea.ConsumirAPI.Controllers
+{
+    [ApiController]
+    [Route("api/artigos")]
+    public class ArtigoController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private string _baseUrl;
+
+        public ArtigoController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _baseUrl = _configuration.GetSection("BaseUrl").Value;
+        }
+
+        public async Task<IActionResult> ObterListaDeArtigos()
+        {
+            using (var httpClient = new HttpClient())
+            {
+                httpClient.BaseAddress = new Uri(_baseUrl);
+
+                var response = await httpClient.GetAsync(_baseUrl);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var json = await response.Content.ReadAsStringAsync();
+                    var artigos = JsonConvert.DeserializeObject<Recurso>(json);
+                    return Ok(artigos);
+                }
+                else
+                    return BadRequest();
+
+            }
+        }
+    }
+}
---
+++ Innovea.ConsumirAPI/Dtos/ArtigoDto.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Innovea.ConsumirAPI.Dtos
+{
+    
+    
+    public class ArtigoDto
+    {
+        [JsonProperty("author")]
+        public string Author { get; set; }
+
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+    }
+
+    public class Recurso
+    {
+        [JsonProperty("articles")]
+        public IEnumerable<ArtigoDto> ArtigoDto { get; set; }      
+    }
+
+
+}
